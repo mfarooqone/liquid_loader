e550d4a4d75e8be3bf9859a90181b1e63794e988
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2025-04-22 15:32:18 +0500

    WIP Liquid Loader

---
+++ example/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_loader/liquid_loader.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Liquid Loader Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Liquid Loader Example'),
+        ),
+        body: Center(
+          child: LiquidLoader(
+            width: 300,
+            height: 500,
+            liquidColor: Colors.blue,
+            borderColor: Colors.grey,
+            capColor: Colors.orange,
+            liquidLevel: 0.6,
+            text: "60%",
+            textStyle: TextStyle(),
+            shape: Shape.rectangle,
+            hideCap: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
